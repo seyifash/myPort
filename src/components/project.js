ed0900d948460cc672c704fe470d937b9b8f77ed
oluwaseyi <99446401+seyifash@users.noreply.github.com>
2025-09-26 11:09:18 +0100

    Update project.js
    Updated portfolio with live projects

--- src/components/project.js
+++ src/components/project.js
@@ -18,22 +18,40 @@ const Projects = () => {
                 </div>
             </div>
             <div className="projects-descrip">
-                <h4>Shoppers</h4>
-                <p>An E-commerce website design that provides an interactive interface creating a seamless user
-                    experience using only HTML Javascript and CSS.
+                <h4>Shoppers – Multi-Vendor E-Commerce Platform</h4>
+                <p>A scalable e-commerce platform where vendors can register, add products, 
+        and track orders while customers browse and purchase seamlessly. 
+        Built with React (Vite) and CSS, with Zustand for state management. Integrated Paystack multisplit payments, 
+        two-factor authentication, vendor dashboards, dark/light mode, and real-time notifications for a smooth shopping experience.
                 </p>
                 <div class="code">
-                    <Link to="https://seyifash.github.io/demo_shoppers/">View Project</Link>
-                    <Link to="https://github.com/seyifash/demo_shoppers">Source Code</Link>
+                    <Link to="https://shopit.buildeapp.org/">View Project</Link>
+                    <Link to="https://github.com/seyifash/MPEcomm.git">Source Code</Link>
                 </div>
             </div>
             <div className="projects-descrip">
-                <h4>PetPulse</h4>
-                <p>An affiliate pet product website built with Next.js and Tailwind CSS. Users can browse and compare pet food and accessories; clicking a product redirects them to the actual affiliate store to make a purchase. Includes an admin dashboard for uploading products and blogs, managing content (CRUD), and updating profiles.
+                <h4>Fastrasuite – ERP Management Platform</h4>
+                <p>A web-based ERP solution designed to streamline business operations such as purchases, invoicing, vendor management, and multi-level approvals. Built with React, Next.js, CSS, and TailwindCSS, using Context API for global state management and role-based access. Delivered modular UI components, approval workflows, and scalable designs integrated with backend APIs for efficient enterprise resource planning.
                 </p>
                 <div class="code">
-                    <Link to="https://petpulse-phi.vercel.app/">View Project</Link>
-                    <Link to="https://github.com/seyifash/petpulse.git">Source Code</Link>
+                    <Link to="https://app.fastrasuite.com/">View Project</Link>
+                </div>
+            </div>
+
+            <div className="projects-descrip">
+                <h4>Elsa David – Portfolio Website</h4>
+                <p>A modern personal portfolio built with React, Vite, and TailwindCSS. Designed with a focus on clean UI/UX and responsive layouts. A website development, showcasing branding and project highlights with a dynamic, scalable frontend architecture</p>
+                <div class="code">
+                    <Link to="https://elsa-dave-portfolio.vercel.app/">View Project</Link>
+                     <Link to="https://github.com/seyifash/ElsaDave-portfolio.git">Source Code</Link>
+                </div>
+            </div>
+            <div className="projects-descrip">
+                <h4>Adeola – Portfolio Website</h4>
+                <p>A fast, SEO-optimized portfolio designed to highlight personal branding and projects. Built with Next.js and pure CSS, emphasizing performance, accessibility, and a polished professional look across devices.</p>
+                <div class="code">
+                    <Link to="https://adeola-portfolio-peach.vercel.app/">View Project</Link>
+                     <Link to="https://github.com/seyifash/Adeola-portfolio.git">Source Code</Link>
                 </div>
             </div>
             </div>
